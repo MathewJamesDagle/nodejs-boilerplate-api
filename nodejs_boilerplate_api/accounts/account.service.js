7100d481cbd91a5dd463fd6032b8373b9846dfef
MathewJamesDagle <126637284+MathewJamesDagle@users.noreply.github.com>
2024-04-02 21:19:01 +0800

    Add files via upload
    Forgot password and reset password functionality

---
+++ nodejs_boilerplate_api/accounts/account.service.js
@@ -0,0 +1,24 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const { Op } = require('sequelize');
+const sendEmail = require('_helpers/send-email');
+const db = require('_helpers/db');
+const Role = require('_helpers/role');
+
+module.exports = {
+    authenticate,
+    refreshToken,
+    revokeToken,
+    register,
+    verifyEmail,
+    forgotPassword,
+    validateResetToken,
+    resetPassword,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+}
