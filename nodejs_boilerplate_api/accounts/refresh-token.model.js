86a1d23189a9d702b79cc229020a5de887e835cb
Mathew James Dagle <mathewdagle@gmail.com>
2024-04-07 23:52:43 +0800

    Final commit

--- nodejs_boilerplate_api/accounts/refresh-token.model.js
+++ nodejs_boilerplate_api/accounts/refresh-token.model.js
@@ -1,23 +1,25 @@
-const { DataTypes, DATE } = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 module.exports = model;
 
+
 function model(sequelize) {
     const attributes = {
-        token: { type: DataTypes.STRING },
-        expires: { type: DataTypes.DATE },
-        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-        createdByIp: { type: DataTypes.STRING },
-        revoked: { type: DataTypes.DATE },
-        revokedByIp: { type: DataTypes.STRING },
-        replacedByToken: { type: DataTypes.STRING },
+        token: {type: DataTypes.STRING},
+        expires: {type: DataTypes.DATE},
+        created: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
+        createdByIp: {type: DataTypes.STRING},
+        revoked: {type: DataTypes.DATE},
+        revokedByIp: {type: DataTypes.STRING},
+        replacedByToken: {type: DataTypes.STRING},
         isExpired: {
             type: DataTypes.VIRTUAL,
-            get() { return DATE.now() >= this.expires; }
+            get() { return Date.now() >= this.expires}
         },
         isActive: {
             type: DataTypes.VIRTUAL,
-            get() { return !this.revoked && !this.isExpired; }
+            get() { return !this.revoked && !this.isExpired;}
+
         }
     };
 
